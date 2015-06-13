e39250f66308bed99b844cb16ae1a57fa53c76b2
mzwiech <6283879@gmail.com>
2015-06-13 13:03:17 +0200

    logic

--- Puzzle WatchKit Extension/InterfaceController.swift
+++ Puzzle WatchKit Extension/InterfaceController.swift
@@ -16,6 +16,9 @@ class InterfaceController: WKInterfaceController {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
+        ButtonsMatrix = [btn1x1, btn1x2, btn1x3,
+        btn2x1, btn2x2, btn2x3,
+        btn3x1, btn3x2, btn3x3]
     }
 
     override func willActivate() {
@@ -82,6 +85,68 @@ class InterfaceController: WKInterfaceController {
     
     func clickLogger(btn: Int) {
         println(String(btn))
+        checkNeighbor(btn)
+        rerenderLabels()
+    }
+    private func swap(index: Int) {
+        matrix[emptyIndex] = matrix[index]
+        matrix[index] = ""
+        emptyIndex = index
+    }
+    private func rerenderLabels() {
+        if let m = ButtonsMatrix {
+            for index in 1...9 {
+                m[index-1].setTitle(matrix[index])
+            }
+        }
     }
     
+    private var matrix : [String] = ["0","1","2","3","4","5","6","7","8",""]
+    private var ButtonsMatrix : [WKInterfaceButton]?
+    private var emptyIndex = 9
+    private func checkNeighbor(index: Int) {
+        switch index {
+        case 1:
+            if contains([2,4], emptyIndex) {
+                swap(index)
+            }
+        case 2:
+            if contains([1,3,5], emptyIndex) {
+                swap(index)
+            }
+        case 3:
+            if contains([2,6], emptyIndex) {
+                swap(index)
+            }
+        case 4:
+            if contains([1,5,7], emptyIndex) {
+                swap(index)
+            }
+        case 5:
+            if contains([2,4,6,8], emptyIndex) {
+                swap(index)
+            }
+        case 6:
+            if contains([3,5,9], emptyIndex) {
+                swap(index)
+            }
+        case 7:
+            if contains([4,8], emptyIndex) {
+                swap(index)
+            }
+        case 8:
+            if contains([5,7,9], emptyIndex) {
+                swap(index)
+            }
+        case 9:
+            if contains([6,8], emptyIndex) {
+                swap(index)
+            }
+        default:
+            break
+            
+        }
+        
+    }
+
 }
